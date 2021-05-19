c8f89184881150c0eb610886a599c020aa8528cf
vanhuuan89 <58378623+vanhuuan89@users.noreply.github.com>
2021-05-19 16:11:58 +0700

    fix request

--- Dtos/User/ResetPassRequest.cs
+++ Dtos/User/ResetPassRequest.cs
@@ -2,7 +2,6 @@ namespace TalktifAPI.Dtos
 {
     public class ResetPassRequest
     {
-        public string Email { get; set; }
-        public string NewPass { get; set; }    
+        public string Email { get; set; }  
     }
 }
