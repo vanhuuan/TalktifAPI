77508ee77cb74e91cf5c0c7ed67f38b6dbef0ced
vanhuuan89 <58378623+vanhuuan89@users.noreply.github.com>
2021-08-06 16:51:03 +0700

    keep testing

--- Repository/ChatRoomRepository.cs
+++ Repository/ChatRoomRepository.cs
@@ -17,7 +17,7 @@ namespace TalktifAPI.Repository
 
         public ChatRoom GetChatRoomByName(string name)
         {
-            return Entities.OrderBy(x => x.Id).LastOrDefault(p => p.ChatRoomName.Equals(name));
+            return Entities.OrderByDescending(x => x.Id).FirstOrDefault(p => p.ChatRoomName.Equals(name));
         }
     }
 }
